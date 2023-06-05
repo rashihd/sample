7f939fe3e4ce2827367f1461a1f61294d8208c65
rashihd <hakimrashid207@gmail.com>
2023-06-05 18:51:29 +0530

    added get api

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 app.get('/',(req,res)=>{
     res.status(200).send("working Fine!!!")
 })
+app.get('/check',(req,res)=>{
+    res.status(200).send("checking")
+})
 
 app.listen(3000,()=>{
     console.log('server is listening on port 3000')
