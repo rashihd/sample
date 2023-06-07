d917611aae4f78fb5a344550e2a023c4b3725439
rashihd <hakimrashid207@gmail.com>
2023-06-07 14:28:17 +0530

    testing ci cd

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const app = express();
 app.get('/',(req,res)=>{
     res.status(200).send("working Fine")
 })
-
+app.get('/info',(req,res)=>{
+    res.status(200).send("testing ci cd")
+})
 app.listen(3000,()=>{
     console.log('server is listening on port 3000')
 })
